5c5db269cd89e8fe6a5d1238740df3dbcdf07a0e
kinetix <kinetixarts@outlook.com>
2019-01-09 15:28:30 +0300

    Hello World

---
+++ MVCMovie/MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public string Welcome(String firstName, String lastName, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Welcome to The Movie app " + firstName + " " + lastName + " " + ID);
+        }
+
+       
+    }
+}
